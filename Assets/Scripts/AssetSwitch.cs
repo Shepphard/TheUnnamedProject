e9af259ffce578c868a19b6dd4657470375b2a50
Vivien <hollowspecder@googlemail.com>
2014-11-07 15:21:16 +0100

    AssetSwitcherNew Script added, ISSUE CLOSED
    1. Refactored the structure a little bit
    2. AssetSwitchNew (dont use AssetSwitch),apply this on an empty gameobj
    optional with a collider, then add two prefabs to the script. look at
    the AssetSwither prefab on how to use it

---
+++ Assets/Scripts/AssetSwitch.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssetSwitch : MonoBehaviour {
+
+	//public Mesh newMesh;
+	public GameObject newObject;
+	
+	private MeshRenderer renderer;
+	private MeshFilter meshfilter;
+	private Material[] oldMaterials;
+	private Material[] newMaterials;
+	private Mesh oldMesh;
+	private Mesh newMesh;
+	
+	void Awake()
+	{
+		meshfilter = GetComponent<MeshFilter>();
+		renderer = GetComponent<MeshRenderer>();
+		
+		oldMesh = meshfilter.mesh;
+		newMesh = newObject.GetComponent<MeshFilter>().mesh;
+		
+		oldMaterials = renderer.materials;
+		newMaterials = newObject.GetComponent<MeshRenderer>().materials;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.J))
+		{
+			meshfilter.mesh = newMesh;
+			//newMesh = oldMesh;
+			oldMesh = meshfilter.mesh;
+			
+			renderer.materials = newMaterials;
+		}
+	}
+}
---
+++ Assets/Scripts/AssetSwitchNew.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssetSwitchNew : MonoBehaviour {
+
+	public GameObject object1;
+	public GameObject object2;
+
+	private GameObject instObject1;
+	private GameObject instObject2;
+	private ParticleSystem cloudParticles;
+	
+	void Awake()
+	{
+		instObject1 = (GameObject) Instantiate(object1, transform.position, transform.rotation);
+		instObject2 = (GameObject) Instantiate(object2, transform.position, transform.rotation);
+
+		instObject1.transform.parent = gameObject.transform;
+		instObject2.transform.parent = gameObject.transform;
+		
+		instObject2.SetActive(false);
+		
+		cloudParticles = GetComponentInChildren<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.J))
+		{
+			instObject2.SetActive(!instObject2.activeSelf);
+			instObject1.SetActive(!instObject1.activeSelf);
+			cloudParticles.Play();
+		}
+	}
+}
