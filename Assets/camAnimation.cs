7c7b79f5f186874029e65fdbb1fc4a652a5117af
Vivien <hollowspecder@googlemail.com>
2014-11-17 22:11:53 +0100

    Added CameraMovement and Footstep SFX

---
+++ Assets/camAnimation.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camAnimation : MonoBehaviour
+{
+	public CharacterController playerController;
+	
+	private Animation _anim; //Empty GameObject's animation component
+	private bool isMoving;
+	private bool left;
+	private bool right;
+	
+	void Awake()
+	{
+		_anim = GetComponent<Animation>();
+	}
+	
+	void CameraAnimations()
+	{
+		if (playerController.isGrounded == true){
+			if (isMoving == true){
+				if (left == true){
+					if(!_anim.isPlaying){//Waits until no animation is playing to play the next
+						_anim.Play("walkLeft");
+						left = false;
+						right = true;
+					}
+				}
+				if (right == true){
+					if (!_anim.isPlaying){
+						_anim.Play("walkRight");
+						right = false;
+						left = true;
+					}
+				}
+			}                  
+		}
+	}
+	
+	void Start () { //First step in a new scene/life/etc. will be "walkLeft"
+		left = true;
+		right = false;
+	}
+	
+	
+	void Update () {
+		//Keyboard input to determine if player is moving
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+		
+		if(inputX  != 0 || inputY != 0)
+			isMoving = true;       
+		else if(inputX == 0 && inputY == 0)
+			isMoving = false;
+		
+		CameraAnimations();
+	}
+}
---
+++ Assets/footstepSounds.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class footstepSounds : MonoBehaviour {
+
+	public AudioClip footstep;
+	public float minVol = 0.5f;
+	public float maxVol = 0.8f;
+	public float minPitch = 0.8f;
+	public float maxPitch = 1.2f;
+	
+	public void PlaceFoot()
+	{
+		float vol = Random.Range(minVol, maxVol);
+		audio.pitch = Random.Range(minPitch, maxPitch);
+		audio.PlayOneShot(footstep, vol);
+	}
+}
