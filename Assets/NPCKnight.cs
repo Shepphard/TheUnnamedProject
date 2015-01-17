39a913e1d53559f97782d350e31300874d4c1ca3
Vivien <hollowspecder@googlemail.com>
2015-01-17 21:06:27 +0100

    Added new wall, fixed NPC Knight

--- Assets/NPCKnight.cs
+++ Assets/NPCKnight.cs
@@ -12,11 +12,18 @@ public class NPCKnight : MonoBehaviour
 	private string[] text;
 	private bool speaking = false;
 	
+	private Equipment _equip;
+	private InteractionControls _ctrl;
+	
 	void Awake()
 	{
 		speech = Speechbubble.Instance();
 		aux = audio;
 		InitText();
+		
+		GameObject player = GameObject.FindGameObjectWithTag(Tags.player);
+		_equip = player.GetComponent<Equipment>();
+		_ctrl = player.GetComponent<InteractionControls>();
 	}
 	
 	public void Interaction()
@@ -36,7 +43,52 @@ public class NPCKnight : MonoBehaviour
 			break;
 		case 2:
 			// talk stuff till all the equipment is there
-		default: StartCoroutine(Talking (7)); break;
+			bool[] equipped = {_equip.wearsItem("helmet", 0),
+							   _equip.wearsItem("shield", 1),
+						       _ctrl.carriesItem("dagger"),
+				               _ctrl.carriesItem("sword")};
+			// does he have everything?
+			bool next = (equipped[0] && equipped[1] && equipped[3]);
+			if (next) {
+				// you have all the equiptment, should ask for name
+				StartCoroutine(Talking(6));
+				interactionStatus++;
+			}
+			// at least one is missing
+			else
+			{
+				int[] possible = new int[3];
+				int count = 0;
+				if (!equipped[0]) {
+					possible[count] = 2;
+					count++;
+				}
+				if (!equipped[1]) {
+					possible[count] = 3;
+					count++;
+				}
+				if (!equipped[3]) {
+					if (!equipped[2]) {
+						possible[count] = 4;
+						count++;
+					}
+					else {
+						possible[count] = 5;
+						count++;
+					}
+				}
+				
+				int picked = Random.Range(0, count);
+				StartCoroutine(Talking(possible[picked]));
+				}
+			break;
+		case 3: 
+			// now lets you in
+			StartCoroutine(Talking(7));
+			animWall.SetTrigger("Open");
+			animWall.collider.enabled = false;
+			break;
+		default: Debug.LogError("Interaction Status is not in switch statement"); break;
 		}
 	}
 	
