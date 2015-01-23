2370ae79b01ef747cea01b229b3c983472e45d06
Vivien <hollowspecder@googlemail.com>
2015-01-23 20:36:22 +0100

    Added Pregame Screen Scene
    With a temporary Logo :)

---
+++ Assets/PreGameScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PreGameScript : MonoBehaviour {
+
+    public int sceneToExitTo = 0;
+
+    public void ExitToScene()
+    {
+        Application.LoadLevel(sceneToExitTo);
+    }
+}
