1dcc056454af3ec6132f14b2bc76c1e46ba7c22e
Vivien <hollowspecder@googlemail.com>
2015-01-25 18:54:06 +0100

    Little Work for StartMenu

---
+++ Assets/MenuCameraRotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuCameraRotation : MonoBehaviour
+{
+    public float rotationSpeed = 3f;
+
+	void Update ()
+    {
+        transform.RotateAround(Vector3.up, rotationSpeed * Time.deltaTime);
+	}
+}
