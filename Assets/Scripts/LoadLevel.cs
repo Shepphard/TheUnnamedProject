cb0808f8e6501fcd83785ace4232354b96ee5ece
Vivien <hollowspecder@googlemail.com>
2015-01-27 18:01:55 +0100

    Added Loading screen and Crossfading!

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour
+{
+    public int Scene;
+
+    void Start()
+    {
+        Application.LoadLevel(Scene);
+    }
+}
---
+++ Assets/Scripts/ScreenFadeInOut.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScreenFadeInOut : MonoBehaviour
+{
+    public float fadeSpeed = 1.5f;
+
+    private bool sceneStarting = true;
+    private bool sceneEnding = false;
+    private Image img;
+    private int sceneToEndTo;
+
+    void Awake()
+    {
+        img = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        if (sceneStarting)
+        {
+            StartScene();
+        }
+        if (sceneEnding)
+        {
+            EndScene();
+        }
+    }
+
+    void FadeToClear()
+    {
+        img.color = Color.Lerp(img.color, Color.clear, fadeSpeed * Time.deltaTime);
+    }
+
+    void FadeToBlack()
+    {
+        img.color = Color.Lerp(img.color, Color.black, fadeSpeed * Time.deltaTime);
+    }
+
+    void StartScene()
+    {
+        FadeToClear();
+
+        if (img.color.a <= 0.05f)
+        {
+            img.color = Color.clear;
+            img.enabled = false;
+            sceneStarting = false;
+        }
+    }
+
+    void EndScene()
+    {
+        FadeToBlack();
+
+        if (img.color.a >= 0.95f)
+        {
+            Application.LoadLevel(sceneToEndTo);
+        }
+
+        Debug.Log("Fading out");
+    }
+
+    public void StartEndScene(int Scene)
+    {
+        sceneToEndTo = Scene;
+        sceneEnding = true;
+        sceneStarting = false;
+        img.enabled = true;
+
+        Debug.Log("Button clicked");
+    }
+}
