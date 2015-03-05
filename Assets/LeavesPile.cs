5682e6e6f9ea22e84adaae7b341efe790ffd84aa
Sushiy <julivizi@yahoo.de>
2015-03-05 17:28:23 +0100

    tweak, added pile of leaves with effect

---
+++ Assets/LeavesPile.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeavesPile : MonoBehaviour 
+{
+	public void OnTriggerEnter(Collider c)
+	{
+		GetComponent<ParticleSystem>().Play();
+	}
+
+}
