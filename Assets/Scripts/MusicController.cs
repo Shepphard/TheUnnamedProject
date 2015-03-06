364d8bd25e97a89c9ce1ddd404fe05449feb5932
Vivien <hollowspecder@googlemail.com>
2015-03-06 12:45:29 +0100

    Started Pauser,not yet working

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -1,89 +1,116 @@
-﻿using UnityEngine;
+﻿﻿using UnityEngine;
 using System.Collections;
 
 public class MusicController : MonoBehaviour
 {
-	public AudioClip maintheme;
-	public AudioClip[] themes;
-	public AudioClip[] sfx;
-	public float fadeSpeed = 2f;
-	public float maxVol = 0.8f;
-	
-	private int sumThemes;
-	private AudioClip lastTheme;
-	private AudioSource _audio;
-	private AudioSource sfxSource;
-	
-	private bool xfade = false;
-	
-	// singleton
-	private static MusicController musicController;
-	public static MusicController Instance() {
-		if (!musicController) {
-			musicController = FindObjectOfType(typeof (MusicController)) as MusicController;
-			if (!musicController)
-				Debug.LogError("There needs to be one active ModalPanelscript on a GameObject in your scene.");
-		}
-		
-		return musicController;
-	}
-	
-	void Awake()
-	{
-		sumThemes = 1 + themes.Length;
-		_audio = audio;
-		lastTheme = maintheme;
-		sfxSource = transform.Find("sfx").GetComponent<AudioSource>();
-		_audio.volume = maxVol;
-	}
-	
-	void Update()
-	{
-		if (!_audio.isPlaying)
-		{
-			lastTheme = _audio.clip;
-			do
-			{
-				int tmp = Random.Range(0, sumThemes*2);
-				if (tmp == 0)
-					_audio.clip = maintheme;
-				else
-					_audio.clip = themes[tmp % 3];
-			} while (lastTheme == _audio.clip);
-			
-			_audio.Play();
-		}
-		
-		MusicFading();
-	}
-	
-	public void PlaySFX(int index)
-	{
-		if (index == 1)
-			fadeSpeed = 5f;
-	
-		xfade = true;
-		sfxSource.clip = sfx[index];
-		sfxSource.Play();
-		Invoke("StopSFX", sfx[index].length);
-	}
-	
-	void StopSFX()
-	{
-		xfade = false;
-	}
-	
-	void MusicFading()
-	{
-		if (xfade)
-		{
-			audio.volume = Mathf.Lerp(audio.volume, 0f, fadeSpeed * Time.deltaTime);
-			sfxSource.volume = Mathf.Lerp(sfxSource.volume, maxVol, fadeSpeed * Time.deltaTime);
-		}
-		else
-		{
-			audio.volume = Mathf.Lerp(audio.volume, maxVol, fadeSpeed * Time.deltaTime);
-			sfxSource.volume = Mathf.Lerp(sfxSource.volume, 0f, fadeSpeed * Time.deltaTime);
-		}
-	}
+    public AudioClip maintheme;
+    public AudioClip[] themes;
+    public AudioClip[] sfx;
+    public float fadeSpeed = 2f;
+    public float maxVol = 0.8f;
+
+    private int sumThemes;
+    private AudioClip lastTheme;
+    private AudioSource _audio;
+    private AudioSource sfxSource;
+    private AudioSource sfxPause;
+
+    private bool xfade = false;
+    private float audioLastTime = 0f;
+
+    // singleton
+    private static MusicController musicController;
+    public static MusicController Instance()
+    {
+        if (!musicController)
+        {
+            musicController = FindObjectOfType(typeof(MusicController)) as MusicController;
+            if (!musicController)
+                Debug.LogError("There needs to be one active ModalPanelscript on a GameObject in your scene.");
+        }
+
+        return musicController;
+    }
+
+    void Awake()
+    {
+        sumThemes = 1 + themes.Length;
+        _audio = audio;
+        lastTheme = maintheme;
+        sfxSource = transform.Find("sfx").GetComponent<AudioSource>();
+        _audio.volume = maxVol;
+        sfxPause = transform.Find("pause").GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (!_audio.isPlaying)
+        {
+            lastTheme = _audio.clip;
+            do
+            {
+                int tmp = Random.Range(0, sumThemes * 2);
+                if (tmp == 0)
+                    _audio.clip = maintheme;
+                else
+                    _audio.clip = themes[tmp % 3];
+            } while (lastTheme == _audio.clip);
+
+            _audio.Play();
+        }
+
+        MusicFading();
+    }
+
+    public void PlaySFX(int index)
+    {
+        if (index == 1)
+            fadeSpeed = 5f;
+
+        xfade = true;
+        sfxSource.clip = sfx[index];
+        sfxSource.Play();
+        Invoke("StopSFX", sfx[index].length);
+    }
+
+    void StopSFX()
+    {
+        xfade = false;
+    }
+
+    void MusicFading()
+    {
+        if (xfade)
+        {
+            audio.volume = Mathf.Lerp(audio.volume, 0f, fadeSpeed * Time.deltaTime);
+            sfxSource.volume = Mathf.Lerp(sfxSource.volume, maxVol, fadeSpeed * Time.deltaTime);
+        }
+        else
+        {
+            audio.volume = Mathf.Lerp(audio.volume, maxVol, fadeSpeed * Time.deltaTime);
+            sfxSource.volume = Mathf.Lerp(sfxSource.volume, 0f, fadeSpeed * Time.deltaTime);
+        }
+    }
+
+    /* takes care of playing the pause music
+     * appropriately! */
+    public void PauseUnpauseMusic(bool paused, float speed)
+    {
+        if (paused)
+        {
+            if (!sfxPause.isPlaying)
+                sfxPause.Play();
+
+            audio.volume = Mathf.Lerp(audio.volume, 0f, speed * 1 / 60);
+            sfxPause.volume = Mathf.Lerp(sfxPause.volume, maxVol, speed * 1 / 60);
+        }
+        else
+        {
+            audio.volume = Mathf.Lerp(audio.volume, maxVol, speed * 1 / 60);
+            sfxPause.volume = Mathf.Lerp(sfxPause.volume, 0f, speed * 1 / 60);
+
+            if (sfxPause.isPlaying && sfxPause.volume <= 0.05f)
+                sfxPause.Stop();
+        }
+    }
 }
---
+++ Assets/Scripts/Pauser.cs
@@ -0,0 +1,62 @@
+﻿﻿using UnityEngine;
+using System.Collections;
+
+public class Pauser : MonoBehaviour
+{
+    public KeyCode key_pause;
+    public GameObject pausePanel;
+    public float musicFadeSpeed = 3f;
+
+    private InteractionControls playerCtrl;
+    private MusicController music;
+
+    private bool paused = false;
+
+    void Awake()
+    {
+        playerCtrl = GameObject.FindGameObjectWithTag(Tags.player).GetComponent<InteractionControls>();
+        music = MusicController.Instance();
+    }
+
+    void Update()
+    {
+        PauseInput();
+
+        music.PauseUnpauseMusic(paused, musicFadeSpeed);
+    }
+
+    void PauseInput()
+    {
+        if (Input.GetKeyDown(key_pause))
+        {
+            if (pausePanel.activeSelf)
+            {
+                Unpause();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Pause()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0;
+        playerCtrl.enabled = false;
+        paused = true;
+
+        Debug.Log("Paused Game");
+    }
+
+    void Unpause()
+    {
+        playerCtrl.enabled = true;
+        Time.timeScale = 1;
+        paused = false;
+        pausePanel.SetActive(false);
+
+        Debug.Log("Unpaused Game");
+    }
+}
