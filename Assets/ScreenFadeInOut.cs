a34aba1e5308c0ecd3f538ab84d62819a3c98187
Vivien <hollowspecder@googlemail.com>
2015-03-06 12:35:28 +0100

    Added newer Startmenu

---
+++ Assets/ScreenFadeInOut.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScreenFadeInOut : MonoBehaviour
+{
+    public float fadeSpeed = 1.5f;
+
+    private bool sceneStarting = true;
+    private bool sceneEnding = false;
+    private Image img;
+    private int sceneToEndTo; // is set to -1 to QUIT
+
+    void Awake()
+    {
+        img = GetComponent<Image>();
+        img.color = Color.black;
+    }
+
+    void Update()
+    {
+        if (sceneStarting)
+        {
+            StartScene();
+        }
+        if (sceneEnding)
+        {
+            EndScene();
+        }
+    }
+
+    void FadeToClear()
+    {
+        img.color = Color.Lerp(img.color, Color.clear, fadeSpeed * Time.deltaTime);
+    }
+
+    void FadeToBlack()
+    {
+        img.color = Color.Lerp(img.color, Color.black, fadeSpeed * Time.deltaTime);
+    }
+
+    void StartScene()
+    {
+        FadeToClear();
+
+        if (img.color.a <= 0.05f)
+        {
+            img.color = Color.clear;
+            img.enabled = false;
+            sceneStarting = false;
+        }
+    }
+
+    void EndScene()
+    {
+        FadeToBlack();
+
+        if (img.color.a >= 0.95f)
+        {
+            if (sceneToEndTo < 0)
+                Application.Quit();
+            Application.LoadLevel(sceneToEndTo);
+        }
+
+        Debug.Log("Fading out");
+    }
+
+    public void StartEndScene(int Scene)
+    {
+        sceneToEndTo = Scene;
+        sceneEnding = true;
+        sceneStarting = false;
+        img.enabled = true;
+
+        Debug.Log("Button clicked");
+    }
+}
---
+++ Assets/Scripts/SwitchLetter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchLetter : MonoBehaviour {
+
+    public Sprite switchOutWith;
+    public float swithTimer = 1f;
+
+    private ParticleSystem cloud;
+    private SpriteRenderer render;
+    private WiggleLetter wiggleScript;
+
+    void Awake()
+    {
+        cloud = GetComponentInChildren<ParticleSystem>();
+        render = GetComponent<SpriteRenderer>();
+    }
+
+	void Start ()
+    {
+        Invoke("SwitchOut", swithTimer);
+	}
+
+    void SwitchOut()
+    {
+        render.sprite = switchOutWith;
+        cloud.Play();
+    }
+
+    public void TurnOnWiggle()
+    {
+        wiggleScript.enabled = true;
+    }
+}
