812ba2b2bd76b3d56e8ba685e29ae735dc0728e9
Vivien <hollowspecder@googlemail.com>
2015-03-07 18:54:09 +0100

    Cutscene: Kamerafahrt fehlt noch

--- Assets/NPCMoonCutscene.cs
+++ Assets/NPCMoonCutscene.cs
@@ -3,13 +3,34 @@ using System.Collections;
 
 public class NPCMoonCutscene : MonoBehaviour {
 
+    public GameObject normal;
+    public GameObject switched;
+
+    bool started = false;
+
+    void Update()
+    {
+        if (started)
+        {
+            if (!audio.isPlaying)
+                Application.LoadLevel(5);
+        }
+    }
+
     public void StartMusic()
     {
         audio.Play();
+        started = true;
     }
 
     public void EnterShip()
     {
         transform.parent.GetComponent<SpaceCutscene>().EnterShip();
+    }
+
+    public void Switch()
+    {
+        normal.SetActive(false);
+        switched.SetActive(true);
     }
 }
--- Assets/SpaceCutscene.cs
+++ Assets/SpaceCutscene.cs
@@ -1,10 +1,46 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class SpaceCutscene : MonoBehaviour {
 
+    public Image img;
+    public float fadeSpeed = 0.05f;
+    public bool endingLevel = false;
+
     public void EnterShip()
     {
         GetComponent<Animator>().SetTrigger("open");
+    }
+
+    public void switchNPC()
+    {
+        transform.GetChild(0).GetComponent<NPCMoonCutscene>().Switch();
+    }
+
+    public void stillstandNPC()
+    {
+        transform.GetChild(0).GetChild(0).GetComponent<Animator>().SetTrigger("StillStanding");
+    }
+
+    public void EndLevel()
+    {
+        endingLevel = true;
+    }
+
+    public void loadNext()
+    {
+        Application.LoadLevel(5);
+    }
+
+    void Update()
+    {
+        if (endingLevel)
+            FadeToBlack();
+    }
+
+    void FadeToBlack()
+    {
+        img.color = Color.Lerp(img.color, Color.black, fadeSpeed * Time.deltaTime);
     }
 }
