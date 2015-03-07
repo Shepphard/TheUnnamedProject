a1e01a5569d06523fa4116703f0c19f223b594f9
Sushiy <julivizi@yahoo.de>
2015-03-07 19:31:50 +0100

    added deco

---
+++ Assets/Scripts/AI_gun.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI_gun : MonoBehaviour 
+{
+
+	private Gun gun;
+	private Transform playerT;
+	private Rigidbody playerR;
+
+
+	public float maxDistance = 20f; //maximum target scan distance
+
+	private float timer = 0f;
+	public float maxTimer = 10f;	//Time between shots
+	// Use this for initialization
+	void Start () 
+	{
+		gun = GetComponent<Gun>();
+		GameObject g = GameObject.FindGameObjectWithTag("Player");
+		playerT = g.transform;
+		playerR = g.rigidbody;
+		timer = maxTimer+1;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(timer <= maxTimer)
+		{
+			timer += Time.deltaTime;
+		}
+		//is the player in Range?
+		float d = Vector3.Distance(transform.position, playerT.position);
+		if(d <= maxDistance)
+		{
+			
+			//look at the player
+			transform.LookAt(playerT);
+
+			//shoot Ray to check if the player is visible
+			RaycastHit hit;
+			LayerMask noBarrierMask = LayerMask.GetMask("barrier");
+			noBarrierMask = ~noBarrierMask;
+			Physics.Raycast(transform.position, transform.forward, out hit, maxDistance, noBarrierMask);
+			if(hit.collider.CompareTag("Player") && timer > maxTimer)
+			{
+				transform.LookAt(playerT.position + (playerR.velocity * d/maxDistance * Random.Range(0.5f, 2f)));
+				timer = 0f;
+				gun.Shoot();
+			}
+		}
+	}
+}
--- Assets/Scripts/Items/Gun.cs
+++ Assets/Scripts/Items/Gun.cs
@@ -9,17 +9,35 @@ public class Gun : MonoBehaviour
 	public Vector3 muzzleOffset = new Vector3(0f, 0.2f, 0.7f);
 	public float strength = 10f;
 
+	private float timer;
+	private float maxTimer = 1.5f;
+
+
 	ParticleSystem particle;
 	// Use this for initialization
 	void Awake()
 	{
 		particle = GetComponentInChildren<ParticleSystem>();
+		timer = maxTimer + 1;
 	}
+
+	void Update()
+	{
+		if(timer < maxTimer)
+		{
+			timer += Time.deltaTime;
+		}
+	}
+
 
 	public void Shoot()
 	{
-		GameObject newProjectile = (GameObject)Instantiate(projectile, transform.position + transform.TransformDirection(muzzleOffset), transform.rotation);
-		newProjectile.rigidbody.AddForce(transform.forward * strength);
-		particle.Play();
+		if(timer >= maxTimer)
+		{
+			GameObject newProjectile = (GameObject)Instantiate(projectile, transform.position + transform.TransformDirection(muzzleOffset), transform.rotation);
+			newProjectile.rigidbody.AddForce(transform.forward * strength);
+			particle.Play();
+			timer = 0f;
+		}
 	}
 }
